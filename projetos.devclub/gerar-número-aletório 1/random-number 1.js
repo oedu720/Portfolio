e2e8f245f5208e17a19a1012e49dcdc54d644981
Eduardo da Silva Gomes <149094866+oedu720@users.noreply.github.com>
2023-11-21 20:49:58 -0300

    Delete projetos.devclub/gerar-número-aletório 1 directory

--- projetos.devclub/gerar-número-aletório 1/random-number 1.js
+++
@@ -1,8 +0,0 @@
-function generateNumber(){
-
-const minValue = Math.ceil (document.querySelector("#first-input").value)
-const maxValue = Math.floor (document.querySelector("#second-input").value)
-const result = Math.floor (Math.random() * (maxValue - minValue + 1)) + minValue
-alert(result)
-}
-
