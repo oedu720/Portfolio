bfbe518b5315ec073f12658c8f3d85fe508ea543
Eduardo da Silva Gomes <149094866+oedu720@users.noreply.github.com>
2025-06-09 13:31:23 -0300

    Delete devburguer-api directory

--- devburguer-api/src/config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-  secret: '975359016138b0223ab789505f7dbeca',
-  expiresIn: '5d',
-};
--- devburguer-api/src/server.js
+++
@@ -1,3 +0,0 @@
-import app from './app';
-
-app.listen(3001, () => console.log('Server is running at port 3001....'));
